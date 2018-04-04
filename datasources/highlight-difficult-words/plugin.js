57b0b90113f0c38956b2dd5b703af04ecad9ab13
Rory Byrne <roryofbyrne@gmail.com>
2018-04-04 16:12:03 +0100

    Removed logs and removed timestamp from datapoint

--- datasources/highlight-difficult-words/plugin.js
+++ datasources/highlight-difficult-words/plugin.js
@@ -1,20 +1,17 @@
 const sendDatapoint = function(text, mc) {
   let datapoint = {};
   datapoint['text'] = text;
-  datapoint['timestamp'] = Date.now();
   console.log(datapoint);
 
   mc.sendDatapoint(datapoint);
 }
 
 const sendDifficultWord = function() {
-  console.log("Callback triggered");
   let text = window.getSelection().toString();
   sendDatapoint(text, mc);
 }
 
 const createMenuButton = function(mc) {
-  console.log('Creating contextMenu button');
   mc.createContextMenuButton({
     buttonFunction: 'highlightDifficultWordsFunction',
     buttonTitle: 'Difficult Word',
@@ -26,7 +23,5 @@ var mc;
 
 function initDataSource(metroClient) {
   mc = metroClient;
-  console.log('Loading difficult-words DataSource...')
   createMenuButton(metroClient);
-  console.log("Done!");
 }
