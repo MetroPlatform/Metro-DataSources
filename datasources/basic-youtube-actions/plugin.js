23d1237edb1a5d31bc99db63098e637dea192b80
Rory Byrne <roryofbyrne@gmail.com>
2019-01-09 16:18:32 +0000

    jQuery-ify the DataSource and fix this/self bug

--- datasources/basic-youtube-actions/plugin.js
+++ datasources/basic-youtube-actions/plugin.js
@@ -4,26 +4,27 @@ const basicYoutubeActions = {
 
   registerEventsHandler: function(node) {
     // First we need to get the video player element:
-    let videoPlayerNode = document.getElementById("movie_player");
+    let $videoPlayer = $('#movie_player');
     // There is a class that is dynamically changed on this element. Either
     // "paused-mode" or "playing-mode" or "ended-mode". We are going to
     // setInterval on this and monitor it for changes to the class. When it
     // changes, we fire that event.
     let currentEvent = "";
+    const self = this;
 
     setInterval(function() {
-      let playing = videoPlayerNode.classList.contains("playing-mode");
-      let paused = videoPlayerNode.classList.contains("paused-mode");
-      let ended = videoPlayerNode.classList.contains("ended-mode");
+      let playing = $videoPlayer.hasClass("playing-mode");
+      let paused = $videoPlayer.hasClass("paused-mode");
+      let ended = $videoPlayer.hasClass("ended-mode");
 
       if(playing && currentEvent != "play") {
-        this.sendDatapoint("play");
+        self.sendDatapoint("play");
         currentEvent = "play";
       } if(paused && currentEvent != "pause") {
-        this.sendDatapoint("pause");
+        self.sendDatapoint("pause");
         currentEvent = "pause";
       } if(ended && currentEvent != "finished") {
-        this.sendDatapoint("finished");
+        self.sendDatapoint("finished");
         currentEvent = "finished";
       }
     }, 250);
@@ -37,15 +38,19 @@ const basicYoutubeActions = {
     datapoint['url'] = window.location.href;
     datapoint['title'] = document.title;
 
+    // Log it
+    console.log(datapoint);
+
     // And ship it off:
     this.mc.sendDatapoint(datapoint);
   },
 
   initDataSource: function(metroClient) {
+    // The entrypoint
     this.mc = metroClient;
-    console.log("Beginning basic-youtube-actions data source.");
+    console.log("Beginning YouTube actions DataSource.");
 
-    // Then start our plugin.
+    // Set up the event listeners on the page.
     this.registerEventsHandler(document.body);
   }
 }
